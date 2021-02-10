70cbe6244e1c2cb5d2dcd24c2e7b2ee317c1116c
jkz1g18 <jkz1g18@soton.ac.uk>
2021-02-10 14:58:00 +0000

    Added simplex II-phase

---
+++ jordan_exchange.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def jordan_exchange(A:np.array,pivot:tuple):
+
+    x,y = pivot
+
+    pivot_col = (A[:,y] / A[x,y]).reshape(-1,1)
+
+
+    B = A[:,:] - pivot_col @ A[x,:].reshape(1,-1)
+
+    B[:,y] = pivot_col.reshape(-1)
+
+    B[x,:] = - A[x,:] / A[x,y]
+
+    B[x,y] = 1 / A[x,y]
+
+    return B
+
+if __name__ == '__main__':
+    
+    A = np.array([[-2,3,6],[-4,-5,20]],dtype=np.float)
+    print(A)
+    print(jordan_exchange(A,(0,0)))
